4988bc01816a63a0672ecf792a782ffd8731e99b
Brad Anderson <brad@metabase.com>
2026-03-26 10:50:47 -0400

    GRO-281 start updating TransformsUpsellPage

--- enterprise/frontend/src/metabase-enterprise/transforms/upsells/pages/TransformsUpsellPage/TransformsUpsellPage.tsx
+++ enterprise/frontend/src/metabase-enterprise/transforms/upsells/pages/TransformsUpsellPage/TransformsUpsellPage.tsx
@@ -1,73 +1,39 @@
-import cx from "classnames";
-import { useState } from "react";
-import { t } from "ttag";
+import { jt, t } from "ttag";
 
 import { LoadingAndErrorWrapper } from "metabase/common/components/LoadingAndErrorWrapper";
 import { DataStudioBreadcrumbs } from "metabase/data-studio/common/components/DataStudioBreadcrumbs";
 import { PaneHeader } from "metabase/data-studio/common/components/PaneHeader";
 import { DottedBackground } from "metabase/data-studio/upsells/components/DottedBackground";
 import { LineDecorator } from "metabase/data-studio/upsells/components/LineDecorator";
-import type { BillingPeriod } from "metabase/data-studio/upsells/types";
 import { useSelector } from "metabase/lib/redux";
 import { getStoreUsers } from "metabase/selectors/store-users";
-import {
-  Card,
-  Center,
-  Divider,
-  Flex,
-  Icon,
-  Stack,
-  Text,
-  Title,
-} from "metabase/ui";
+import { Button, Card, Center, Flex, Stack, Text, Title } from "metabase/ui";
 
 import { useTransformsBilling } from "../../hooks/useTransformsBilling";
 
 import S from "./TransformsUpsellPage.module.css";
-import { PricingSummary } from "./components/PricingSummary";
-import { TierSelection, type TransformTier } from "./components/TierSelection";
 
 /**
- * Expected scenarios:
- * - Trial user with no transforms: show both tiers with radio selection, $0 due today, CTA as "Add to trial"
- * - EE user with no transforms, no trial: show both tiers with radio selection, $X due today, CTA as "Add to plan"
- *   - When trial is available for this add-on, show $0 due today, CTA as "Start trial"
- *
  * Note: this upsell page should only be displayed to cloud customers since OSS and Self-hosted have
  * transforms enabled by default.
  */
 export function TransformsUpsellPage() {
-  const bulletPoints = [
-    t`Schedule and run transforms as groups with jobs`,
-    t`Fast runs with incremental transforms that respond to data changes`,
-    t`Predictable costs -  72,000 successful transform runs included every month`,
-    t`If you go over your cap, transforms bill at 0.01 per transform run`,
-  ];
+  const { isStoreUser } = useSelector(getStoreUsers);
 
-  const { isStoreUser, anyStoreUserEmailAddress } = useSelector(getStoreUsers);
-
-  const [selectedTier, setSelectedTier] = useState<TransformTier>("basic");
-
+  // TODO: Check for unused props in useTransformsBilling
   const {
     advancedTransformsAddOn,
     basicTransformsAddOn,
     billingPeriodMonths,
     error,
-    hadTransforms,
     isLoading,
-    isOnTrial,
   } = useTransformsBilling();
 
   const hasData =
     billingPeriodMonths !== undefined &&
     (basicTransformsAddOn || advancedTransformsAddOn);
-  const billingPeriod: BillingPeriod =
-    billingPeriodMonths === 1 ? "monthly" : "yearly";
 
-  const basicTransformsPrice = basicTransformsAddOn?.default_base_fee ?? 0;
-  const advancedTransformsPrice =
-    advancedTransformsAddOn?.default_base_fee ?? 0;
-
+  // TODO: Should there be an admin check?
   const canUserPurchase = hasData && isStoreUser;
 
   if (error || isLoading) {
@@ -92,20 +58,10 @@ export function TransformsUpsellPage() {
     );
   }
 
-  let availableTrialDays =
-    selectedTier === "basic"
-      ? (basicTransformsAddOn?.trial_days ?? 0)
-      : (advancedTransformsAddOn?.trial_days ?? 0);
-
-  if (hadTransforms) {
-    availableTrialDays = 0;
-  }
-
-  const rightColumnTitle = getRightColumnTitle(isOnTrial, availableTrialDays);
-  const selectTierPrice =
-    selectedTier === "basic" ? basicTransformsPrice : advancedTransformsPrice;
-  const dueTodayAmount =
-    availableTrialDays > 0 || isOnTrial ? 0 : selectTierPrice;
+  const freeUnits = 1000; // TODO: Get from api
+  const freeUnitsStr = freeUnits.toLocaleString();
+  const notifyThreshold = 0.8; // TODO: Get from api
+  const perTransformRate = "TODO"; // TODO: Get from api
 
   return (
     <DottedBackground px="3.5rem" pb="2rem">
@@ -121,78 +77,36 @@ export function TransformsUpsellPage() {
         py="xl"
       >
         <LineDecorator>
-          <Card
-            className={cx(S.container, { [S.singleColumn]: !canUserPurchase })}
-            withBorder
-          >
-            <Stack gap="lg" className={S.leftColumn} p="xl">
-              <Title order={2}>
-                {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
-                {t`Start transforming your data in Metabase`}
-              </Title>
-              <Text c="text-secondary">
-                {/* eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins. */}
-                {t`Clean up, reshape, and reuse data directly in Metabase. Add transforms to build models your whole team can use.`}
-              </Text>
-              <Stack gap="lg" py="sm">
-                {bulletPoints.map((point) => (
-                  <Flex direction="row" gap="sm" key={point}>
-                    <Center w={24} h={24}>
-                      <Icon name="check_filled" size={16} c="text-brand" />
-                    </Center>
-                    <Text c="text-secondary">{point}</Text>
-                  </Flex>
-                ))}
-              </Stack>
-              {!canUserPurchase && (
-                <Text fw="bold">
-                  {anyStoreUserEmailAddress
-                    ? // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
-                      t`Please ask a Metabase Store Admin (${anyStoreUserEmailAddress}) to enable this for you.`
-                    : // eslint-disable-next-line metabase/no-literal-metabase-strings -- This string only shows for admins.
-                      t`Please ask a Metabase Store Admin to enable this for you.`}
-                </Text>
+          <Card withBorder p="3rem" w="40rem">
+            <Stack gap="lg" align="start">
+              <Title order={2}>{t`${freeUnitsStr} free transform runs`}</Title>
+              <Text
+                c="text-secondary"
+                fz="1rem"
+                lh={1.4}
+              >{jt`Your cloud plan comes with ${freeUnitsStr} transform runs ${(<strong key="bold">{t`completely free`}</strong>)}. After you use your ${freeUnitsStr} runs you'll be charged ${perTransformRate} per run. You only pay for what you use.`}</Text>
+              <Text
+                c="text-secondary"
+                fz="1rem"
+                lh={1.4}
+              >{t`We'll notify you when you've hit ${notifyThreshold * 100}% of your allotment.`}</Text>
+              {canUserPurchase && (
+                <>
+                  <Button
+                    // TODO
+                    // loading={updateSettingLoading}
+                    variant="primary"
+                    // onClick={enableTransforms}
+                  >{t`Agree and continue`}</Button>
+                </>
               )}
+              <Text c="text-secondary" lh={1.4}>
+                {t`By clicking agree and continue you agree to be charged in accordance with our terms of service. Your free transforms never expire, so they'll be waiting here for you when you'r ready.`}
+              </Text>
             </Stack>
-            {canUserPurchase && (
-              <>
-                <Divider orientation="vertical" />
-                <Stack gap="lg" className={S.rightColumn} p="xl">
-                  <Title order={3}>{rightColumnTitle}</Title>
-                  <TierSelection
-                    advancedTransformsPrice={advancedTransformsPrice}
-                    basicTransformsPrice={basicTransformsPrice}
-                    billingPeriod={billingPeriod}
-                    selectedTier={selectedTier}
-                    setSelectedTier={setSelectedTier}
-                  />
-                  <PricingSummary
-                    dueTodayAmount={dueTodayAmount}
-                    isOnTrial={isOnTrial}
-                    selectedTier={selectedTier}
-                  />
-                </Stack>
-              </>
-            )}
           </Card>
         </LineDecorator>
       </Flex>
     </DottedBackground>
   );
 }
-
-// Determine the title based on scenario
-const getRightColumnTitle = (
-  isOnTrial: boolean,
-  availableTrialDays: number,
-) => {
-  if (isOnTrial) {
-    return t`Add transforms to your trial`;
-  }
-
-  if (availableTrialDays > 0) {
-    return t`Start a free ${availableTrialDays}-day trial of transforms`;
-  }
-
-  return t`Add transforms to your plan`;
-};
